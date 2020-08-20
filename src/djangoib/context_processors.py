7498ec51c8c6e6798d84987b58995b866b5d2059
Mezbaul Haque <mezba109@gmail.com>
2020-08-20 20:54:45 +0600

    Remove google analytics

--- src/djangoib/context_processors.py
+++ src/djangoib/context_processors.py
@@ -7,7 +7,6 @@ APP_NAME = getattr(settings, 'APP_NAME')
 CURRENT_YEAR = datetime.now().year
 COPYRIGHT_TEMPLATE = getattr(settings, 'COPYRIGHT_TEMPLATE')
 COPYRIGHT_TEXT = COPYRIGHT_TEMPLATE.format(APP_NAME=APP_NAME, CURRENT_YEAR=CURRENT_YEAR)
-GA_TRACKING_ID = getattr(settings, 'GA_TRACKING_ID')
 IS_PROD = getattr(settings, 'IS_PROD')
 
 
@@ -15,7 +14,6 @@ def defaults(_):
     ctx = {
         'app_name': APP_NAME,
         'copyright_text': COPYRIGHT_TEXT,
-        'ga_tracking_id': GA_TRACKING_ID,
         'is_production': IS_PROD,
     }
     return ctx
